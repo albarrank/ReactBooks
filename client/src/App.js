ac058c9de6ab55127d5e948906cd063cd29b4d9e
Jack Frost <albarrankevin97@gmail.com>
2019-06-17 15:07:11 -0700

    getting/rendering books in results section

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./pages/Search";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
